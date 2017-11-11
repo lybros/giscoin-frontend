9c0b48fa26337f941d51b3836b79c23f0020e797
Ilya Tkachou <ilyatkachou@gmail.com>
2017-11-12 00:21:35 +0300

    Remove extra spaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { NgModule }              from '@angular/core';
-import { RouterModule, Routes }  from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
  
 import { AppComponent } from './app.component';
-import { ChartsComponent }   from './charts/charts.component';
+import { ChartsComponent } from './charts/charts.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
  
 const appRoutes: Routes = [
