7f2eb619e5e309fb804d659311519114f7340b12
Uladzislau97 <VSvasilyev97@gmail.com>
2017-11-12 17:36:27 +0300

    Redirect to welcome page unless logged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+    if (!this.tokenPresent())
+      this.router.navigateByUrl('/');
+  }
 
   ngOnInit(): void {
   }
